d2df5af1363454b33ce57b27540c8dda0b0f47b3
adrieniisc <106159275+adrieniisc@users.noreply.github.com>
2025-06-04 15:05:53 +0200

    Update Busboy file event signature

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -26,8 +26,8 @@ exports.handler = (event, context, callback) => {
   let fileName = '';
 
   // When Busboy finds a file
-  busboy.on('file', (fieldname, file, filename) => {
-    fileName = filename;
+  busboy.on('file', (fieldname, file, info) => {
+    fileName = info.filename;
     const chunks = [];
     file.on('data', (data) => {
       chunks.push(data);
